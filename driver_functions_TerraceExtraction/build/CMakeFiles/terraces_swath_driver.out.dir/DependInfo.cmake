be78db935dca943d5dd58b180a3050a29c6bba9a
fclubb <f.clubb@ed.ac.uk>
2017-03-12 19:38:26 +0000

    Fixing gitignores

---
+++ driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDChannel.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDChannel.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDCloudBase.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDCloudBase.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDFlowInfo.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDFlowInfo.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDIndexChannel.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDIndexChannel.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDIndexRaster.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDIndexRaster.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDJunctionNetwork.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDJunctionNetwork.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDMostLikelyPartitionsFinder.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDMostLikelyPartitionsFinder.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDParameterParser.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDParameterParser.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDRaster.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDRaster.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDShapeTools.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDShapeTools.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDSpatialCSVReader.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDSpatialCSVReader.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDStatsTools.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDStatsTools.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDSwathProfile.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDSwathProfile.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDTerrace.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/LSDTerrace.cpp.o"
+  "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/terraces_swath_driver.cpp" "/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/build/CMakeFiles/terraces_swath_driver.out.dir/home/s0923330/Datastore/GitHub/LSDTopoTools_TerraceExtraction/driver_functions_TerraceExtraction/terraces_swath_driver.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "qh_QHpointer"
+  "vtkFiltersStatistics_AUTOINIT=1(vtkFiltersStatisticsGnuR)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/vtk"
+  "/usr/include/freetype2"
+  "/usr/include/jsoncpp"
+  "/usr/include/libxml2"
+  "/usr/include/python2.7"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/qhull"
+  "../.."
+  "../../TNT"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
